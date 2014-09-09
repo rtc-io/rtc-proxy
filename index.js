c311a555ccf376b26394fae9ba93fcfce6924802
Damon Oehlman <damon.oehlman@gmail.com>
2014-09-09 16:49:14 +1000

    Properly pass through event handling

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ var EventEmitter = require('eventemitter3');
 
 module.exports = function(prot, methods, attributes, events) {
 
+  function capEvents(target, name) {
+    var events = emitter(target);
+
+    if (typeof target.__orig['on' + name] != 'function') {
+      target.__orig['on' + name] = function() {
+        events.emit.apply(events, [name].concat([].slice.call(arguments)));
+      };
+    }
+
+    return events;
+  }
+
   function emitter(target) {
     if (! target.__emitter) {
       target.__emitter = new EventEmitter();
@@ -11,18 +23,20 @@ module.exports = function(prot, methods, attributes, events) {
   }
 
   // patch methods
-  methods.forEach(function(method) {
-    target[method] = function() {
+  (methods || []).forEach(function(method) {
+    prot[method] = function() {
       var fn = this.__orig && this.__orig[method];
 
       if (typeof fn == 'function') {
         return fn.apply(this.__orig, arguments);
       }
+
+      console.warn('could not invoke: ' + method);
     };
   });
 
   // patch properties
-  attributes.forEach(function(attrData) {
+  (attributes || []).forEach(function(attrData) {
     var key = attrData[0];
     var readonly = attrData[1];
     var gs = {
@@ -40,20 +54,26 @@ module.exports = function(prot, methods, attributes, events) {
     Object.defineProperty(prot, key, gs);
   });
 
-  events.forEach(function(name) {
+  (events || []).forEach(function(name) {
     Object.defineProperty(prot, 'on' + name, {
       get: function() {
         return emitter(this).listeners(name)[0];
       },
 
       set: function(handler) {
-        emitter(this).on(name, handler);
+        var events = capEvents(this, name);
+
+        if (! handler) {
+          return events.removeAllListeners(name);
+        }
+
+        events.on(name, handler);
       }
     });
   });
 
   prot.addEventListener = function(name, handler) {
-    emitter(this).on(name, handler);
+    capEvents(this, name).on(name, handler);
   };
 
   prot.removeEventListener = function(name, handler) {
